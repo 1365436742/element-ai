b61edf39bcdda4961ea551ce4d36f0d7695fa0d0
reqingzai <1365436742@qq.com>
2025-12-18 22:52:35 +0800

    feat: 添加基础sender组件

--- packages/components/sender/index.vue
+++ packages/components/sender/index.vue
@@ -1,16 +1,22 @@
 <template>
-  <div
-    :style="{
-      'min-height': placeholderHeight,
-      opacity,
-    }"
-  >
-    <InputTagPrefix
-      v-if="showInputTagPrefix && iputTagPrefixValue"
-      :value="iputTagPrefixValue"
-      @remove="closeInputTagPrefix"
-    ></InputTagPrefix>
-    <editor-content :editor="editor" />
+  <div :class="ns.b()">
+    <BaseInput
+      v-bind="props"
+      ref="baseInputRef"
+      @update:modelValue="(val) => emits('update:modelValue', val)"
+      @update:showInputTagPrefix="
+        (val) => emits('update:showInputTagPrefix', val)
+      "
+      @enterPressed="emits('enterPressed')"
+      @paste="(event) => emits('paste', event)"
+      @pasteFile="(files) => emits('pasteFile', files)"
+      @blur="emits('blur')"
+      @focus="emits('focus')"
+    >
+      <template #input-tag-prefix>
+        <slot name="input-tag-prefix"></slot>
+      </template>
+    </BaseInput>
   </div>
 </template>
 
@@ -18,140 +24,21 @@
 defineOptions({
   name: 'ElASender',
 })
-import { watch, ref } from 'vue'
-import { useEditor, EditorContent } from '@tiptap/vue-3'
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
-import HardBreak from '@tiptap/extension-hard-break'
-import History from '@tiptap/extension-history'
-import Placeholder from '@tiptap/extension-placeholder'
-import { EditorView } from '@tiptap/pm/view'
-import {
-  handleSenderPasteLogic,
-  getParseFile,
-} from '@element-ai-vue/utils/sender'
-import InputTagPrefix from './input-tag-prefix.vue'
+import { useNamespace } from '@element-ai-vue/hooks'
+import BaseInput from './base-input/index.vue'
+import { BaseInputEmitsType } from './base-input/props'
 import { senderProps } from './props'
+import { useTemplateRef } from 'vue'
 
+const ns = useNamespace('sender')
 const props = defineProps({
   ...senderProps,
 })
 
-const emits = defineEmits<{
-  (e: 'update:modelValue', value: string): void
-  (e: 'update:showInputTagPrefix', value: boolean): void
-  (e: 'enterPressed'): void
-  (e: 'paste', event: ClipboardEvent): void
-  (e: 'pasteFile', files: File[]): void
-  (e: 'blur'): void
-  (e: 'focus'): void
-}>()
-
-const placeholderHeight = ref('')
-const opacity = ref(0)
-
-const handleKeyDown = (view: EditorView, event: KeyboardEvent) => {
-  // 如果是退格键，判断内容是否为空
-  if (event.key === 'Backspace') {
-    // 如果内容为空或仅包含空白字符，阻止删除操作
-    if (editor.value?.isEmpty && props.iputTagPrefixValue) {
-      emits('update:showInputTagPrefix', false)
-    }
-  }
-  if (props.onHandleKeyDown) {
-    return props.onHandleKeyDown(view, event)
-  }
-  if (event.key === 'Enter' && !props.enterBreak) {
-    if (event.shiftKey) {
-      return false
-    }
-    emits('enterPressed')
-    event.preventDefault()
-    return true
-  }
-  return false
-}
-
-const editor = useEditor({
-  content: '',
-  editable: !props.disabled,
-  extensions: [
-    Document,
-    Paragraph,
-    Text,
-    HardBreak,
-    History,
-    Placeholder.configure({
-      placeholder: () => props.placeholder,
-      showOnlyWhenEditable: false,
-    }),
-    ...props.extensions,
-  ],
-  editorProps: {
-    handleKeyDown,
-    handlePaste: handleSenderPasteLogic,
-  },
-  onUpdate: () => {
-    emits('update:modelValue', editor.value?.getHTML() || '')
-  },
-  onCreate() {
-    if (editor.value?.isEmpty) {
-      placeholderHeight.value = getPlaceholderHeight()
-    }
-    editor.value?.commands.setContent(props.modelValue || '')
-    opacity.value = 1
-  },
-  onPaste: (e) => {
-    emits('paste', e)
-    const files = getParseFile(e)
-    if (files?.length) {
-      emits('pasteFile', files)
-    }
-  },
-  onBlur: () => emits('blur'),
-  onFocus: () => emits('focus'),
-})
-
-const getPlaceholderHeight = () => {
-  const placeholder = editor.value?.view.dom.querySelector(
-    '[data-placeholder]'
-  ) as HTMLElement
-  if (placeholder) {
-    const style = window.getComputedStyle(placeholder, '::before')
-    return style.height || '0px'
-  }
-  return ''
-}
-
-const closeInputTagPrefix = () => {
-  emits('update:showInputTagPrefix', false)
-}
-
-watch(
-  () => props.disabled,
-  () => {
-    editor.value?.setEditable(!props.disabled)
-  }
-)
-
-watch(
-  () => props.placeholder,
-  () => {
-    editor.value?.view.dispatch(editor.value.state.tr)
-  }
-)
-
-watch(
-  () => props.modelValue,
-  (newContent) => {
-    const isSame = editor.value?.getHTML() === newContent
-    if (isSame) return
-    editor.value?.commands.setContent(newContent)
-  }
-)
+const emits = defineEmits<BaseInputEmitsType>()
+const baseInputRef = useTemplateRef('baseInputRef')
 
 defineExpose({
-  editor,
+  ...baseInputRef.value,
 })
 </script>
--- packages/components/sender/props.ts
+++ packages/components/sender/props.ts
@@ -1,41 +1,8 @@
-import { Extensions } from '@tiptap/vue-3'
-import { EditorView } from '@tiptap/pm/view'
 import { ExtractPropTypes, PropType } from 'vue'
+import { baseInputProps } from './base-input/props'
 
 export const senderProps = {
-  placeholder: {
-    type: String,
-    default: '',
-  },
-  disabled: {
-    type: Boolean,
-    default: false,
-  },
-  extensions: {
-    type: Array as PropType<Extensions>,
-    default: () => [],
-  },
-  iputTagPrefixValue: {
-    type: String,
-    default: '',
-  },
-  enterBreak: {
-    type: Boolean,
-    default: false,
-  },
-  modelValue: {
-    type: String,
-    default: '',
-  },
-  showInputTagPrefix: {
-    type: Boolean,
-    default: false,
-  },
-  onHandleKeyDown: {
-    type: Function as PropType<
-      (view: EditorView, event: KeyboardEvent) => void
-    >,
-  },
+  ...baseInputProps,
 }
 
 export type SenderPropsType = PropType<
